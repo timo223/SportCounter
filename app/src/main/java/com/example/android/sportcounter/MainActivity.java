05bac7ab58e69a79cfc51f2524f18c0df318ed6b
timo223 <timo.tischer@42digital.de>
2016-08-19 09:13:52 +0200

    addet image

--- app/src/main/java/com/example/android/sportcounter/MainActivity.java
+++ app/src/main/java/com/example/android/sportcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.sportcounter;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void openChecklist(View view) {
         Intent i = new Intent(this, ChecklistActivity.class);
+        startActivity(i);
+    }
+
+    public void openNews(View view){
+        String url = "http://www.sport.de/";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
         startActivity(i);
     }
 }
