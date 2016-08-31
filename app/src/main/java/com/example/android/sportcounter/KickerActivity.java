b21be00d7289d107a3ba1aade5ff912f11686219
timo223 <timo.tischer@42digital.de>
2016-08-31 10:42:52 +0200

    Icons hinzugefügt

--- app/src/main/java/com/example/android/sportcounter/KickerActivity.java
+++ app/src/main/java/com/example/android/sportcounter/KickerActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.sportcounter;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +17,7 @@ public class KickerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kicker);
         displayForTeamA(0);
+
     }
 
     public void minusScoreA(View v) {
