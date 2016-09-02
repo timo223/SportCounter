f7f0fed8fd412c70fa7a44ed4804388be7771321
timo223 <timo.tischer@42digital.de>
2016-09-02 13:16:09 +0200

    about

--- app/src/main/java/com/example/android/sportcounter/AboutActivity.java
+++ app/src/main/java/com/example/android/sportcounter/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.sportcounter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void openIconfinder(View view) {
+        String url = "https://www.iconfinder.com/";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
